6cea6c672946ce8a6c25c1b1241b6373ed9322c9
jorgeeksksj <gabrielliduino@gmail.com>
2025-06-09 14:58:25 -0300

    organizsando snv

--- frontend/src/scripts/app.js
+++
@@ -1,7 +0,0 @@
-// This file contains the main JavaScript logic for the frontend application.
-
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('Frontend application is running.');
-
-    // Add your client-side JavaScript code here
-});
